1a905c87457210c8ba4e26d85859740e195aad97
ncalderon <nathaniel.calderon@gmail.com>
2020-04-18 17:23:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+[Dataset](https://www.kaggle.com/nathanlauga/nba-games-eda-let-s-dive-into-the-data)
---
+++ test_nba_game_processor.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+import nba_game_processor
+
+class Test(TestCase):
+    def test_transform(self):
+        nba_game_processor.transform()
