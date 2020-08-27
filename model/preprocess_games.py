8ef8a1403f625741dc829d5ca89a997844dac9fb
ncalderon <nathaniel.calderon@gmail.com>
2020-08-27 02:56:33 +0200

    sending changes

--- model/preprocess_games.py
+++ model/preprocess_games.py
@@ -215,8 +215,80 @@ def __get_matchup_report(game, previous_games):
     return game_processed
 
 
-def __change_column_order():
-    pass
+def __change_column_order(games_matchup_report_df):
+    return games_matchup_report_df[[
+        "GAME_ID",
+        "GAME_DATE_EST",
+        "HOME_TEAM_NAME",
+        "HOME_TEAM_ID",
+        "VISITOR_TEAM_NAME",
+        "VISITOR_TEAM_ID",
+        "GAME_STATUS_TEXT",
+        "SEASON",
+        "HT_RANK",
+        "HT_CLASS",
+        "HT_HW",
+        "HT_HL",
+        "HT_VW",
+        "HT_VL",
+        "HT_LAST10_W",
+        "HT_LAST10_L",
+        "HT_LAST10_MATCHUP_W",
+        "HT_LAST10_MATCHUP_L",
+        "HT_OVERALL_OFF_POINTS",
+        "HT_OVERALL_DEF_POINTS",
+        "HT_OVERALL_OFF_FG",
+        "HT_OVERALL_DEF_FG",
+        "HT_OVERALL_OFF_3P",
+        "HT_OVERALL_DEF_3P",
+        "HT_OVERALL_OFF_FT",
+        "HT_OVERALL_DEF_FT",
+        "HT_OVERALL_OFF_REB",
+        "HT_OVERALL_DEF_REB",
+        "HT_AWAY_POINTS",
+        "HT_AWAY_FG",
+        "HT_AWAY_3P",
+        "HT_AWAY_FT",
+        "HT_AWAY_REB",
+        "VT_RANK",
+        "VT_CLASS",
+        "VT_HW",
+        "VT_HL",
+        "VT_VW",
+        "VT_VL",
+        "VT_LAST10_W",
+        "VT_LAST10_L",
+        "VT_LAST10_MATCHUP_W",
+        "VT_LAST10_MATCHUP_L",
+        "VT_OVERALL_OFF_POINTS",
+        "VT_OVERALL_DEF_POINTS",
+        "VT_OVERALL_OFF_FG",
+        "VT_OVERALL_DEF_FG",
+        "VT_OVERALL_OFF_3P",
+        "VT_OVERALL_DEF_3P",
+        "VT_OVERALL_OFF_FT",
+        "VT_OVERALL_DEF_FT",
+        "VT_OVERALL_OFF_REB",
+        "VT_OVERALL_DEF_REB",
+        "VT_AWAY_POINTS",
+        "VT_AWAY_FG",
+        "VT_AWAY_3P",
+        "VT_AWAY_FT",
+        "VT_AWAY_REB",
+        "PTS_home",
+        "FG_PCT_home",
+        "FT_PCT_home",
+        "FG3_PCT_home",
+        "AST_home",
+        "REB_home",
+        "PTS_away",
+        "FG_PCT_away",
+        "FT_PCT_away",
+        "FG3_PCT_away",
+        "AST_away",
+        "REB_away",
+        "HOME_TEAM_WINS"
+    ]]
 
 
 def __get_games_matchup_report(season_games: DataFrame, until_season: int = 2015):
@@ -238,6 +310,7 @@ if __name__ == "__main__":
     load_datasets()
     games_matchup_report_df: DataFrame = pd.DataFrame(
         __get_games_matchup_report(season_games=season_games, until_season=2015))
+    games_matchup_report_df = __change_column_order(games_matchup_report_df)
     games_matchup_report_df.to_feather(GAMES_PROCESSED_DS)
     games_matchup_report_df.to_csv(GAMES_PROCESSED_DS_CSV)
     print("Process done")
